1d5ca9d112b5edfb93dc9077876cecf9b6621546
Paolo De Stefani <paolo@paolodestefani.it>
2026-09-10 22:39:15 +0200

    pyInstaller windows batch

--- dist.bat
+++ dist.bat
@@ -1,27 +1,23 @@
-REM creazione pacchetto per pySagra 3.0
+@ECHO OFF
 
-REM utilizzo di un virtual enviroment
+ECHO move to projec root
+cd C:\pyWare\pySagra
 
-REM CD \PyWare\.venv\pyqt5psycopg2\Scripts\Activate.bat
+ECHO activate virtual env
+call "C:\PyWare\.venv\pysidepsycopg\Scripts\activate.bat"
 
-REM CD \PyWare\pySagra\2.0
-
-REM creazione pacchetto su file unico
-REM pyinstaller -w -F --icon pySagra.ico --paths "C:\Python35-32\Lib\site-packages\PyQt5\Qt\bin" --windowed pySagra.py
-
-REM creazione pacchetto su directory
-REM pyinstaller --clean --onedir --icon pySagra.ico --paths "C:\Python37-32\Lib\site-packages\PyQt5\Qt\bin" --windowed pySagra.py
-pyinstaller --clean ^
-	--onedir ^
-	--icon pySagra.ico ^
-	--windowed ^
-	--exclude-module PyQt5.QtDBus ^
-	--exclude-module PyQt5.QtQml ^
-	--exclude-module PyQt5.QtQuick ^
-	pySagra.py
+ECHO create pyInstaller package
+pyinstaller --clean^
+ --onedir^
+ --icon Icon\pySagra.ico^
+ --windowed^
+ --exclude-module PySide6.QtDBus^
+ --exclude-module PySide6.QtQml^
+ --exclude-module PySide6.QtQuick^
+ pySagra.py
 	
+\PyWare\.venv\pysidepsycopg\Scripts\deactivate.bat
 
 pause
 
-REM Deactivate.bat
 
